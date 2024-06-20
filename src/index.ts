dd2294d1df3a9f837e14a1e30817b918d9633369
diyfr <lgs.sgs2@gmail.com>
2024-06-20 12:01:01 +0200

    Format

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,7 @@ export default abstract class BaseComponent {
   /** 
     WIP
   */
-  public onInit: () => void = () => {
-  };
-
+  public onInit: () => void = () => {};
 
   public remove(parent?: HTMLElement | BaseComponent): Promise<void> {
     return new Promise((resolve, _reject) => {
@@ -49,12 +47,16 @@ export default abstract class BaseComponent {
   /** 
     WIP
   */
-  public resetTextEvent: (text: string) => void = (_text: string) => { };
+  public resetTextEvent: (text: string) => void = (_text: string) => {};
 
   /** 
     WIP
   */
-  public classListUpdate(element: HTMLElement, className: string, add: boolean) {
+  public classListUpdate(
+    element: HTMLElement,
+    className: string,
+    add: boolean,
+  ) {
     if (!element.classList.contains(className) && add) {
       element.classList.add(className);
     } else if (element.classList.contains(className) && !add) {
@@ -63,16 +65,16 @@ export default abstract class BaseComponent {
   }
 
   /**
- * Permet d'attendre que l'élément HTML soit vraiment disponible dans le DOM
- * @param itemId Id de l'élément HTML
- * @returns HTMLElement
- */
+   * Permet d'attendre que l'élément HTML soit vraiment disponible dans le DOM
+   * @param itemId Id de l'élément HTML
+   * @returns HTMLElement
+   */
   public waitForRendering(itemId: string): Promise<HTMLElement | null> {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       if (document.getElementById(itemId)) {
         return resolve(document.getElementById(itemId));
       }
-      const observer = new MutationObserver(_mutations => {
+      const observer = new MutationObserver((_mutations) => {
         if (document.getElementById(itemId)) {
           observer.disconnect();
           resolve(document.getElementById(itemId));
@@ -80,7 +82,7 @@ export default abstract class BaseComponent {
       });
       observer.observe(document.body, {
         childList: true,
-        subtree: true
+        subtree: true,
       });
     });
   }
