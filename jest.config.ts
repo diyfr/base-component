1fe33b2d7f4147d832250dbab5391b4efca5c965
Stéphane <diyfr@users.noreply.github.com>
2024-06-20 08:47:04 +0200

    Create jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest',{
+      tsconfig: 'tsconfig.json'
+    }]
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  }
+};
+
+export default config;
